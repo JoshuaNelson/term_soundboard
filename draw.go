3a3e52890b1c9f13745e3af64bc049789a0a4b25
Joshua Nelson <joshua@nelsn.net>
2018-08-10 18:59:09 -0700

    Cleanup tree draw code.
    Moved draw code to draw.go, fixed a few bugs related to extending subtrees
    at the tail end of the parent tree and how the extension lines would be
    populated.

---
+++ draw.go
@@ -0,0 +1,92 @@
+package main
+
+import "github.com/nsf/termbox-go"
+
+var draw_margin int = 2
+var draw_y int = draw_margin - 1
+var v_header_width = 18
+
+func reset_draw() {
+    draw_y = draw_margin - 1
+}
+
+func draw_cell(x, y int, ch rune) {
+	termbox.SetCell(x, y, ch, termbox.ColorWhite, termbox.ColorDefault)
+}
+
+func draw_soundboard(header string, vtree []*voice_tree) {
+	draw_h1(header)
+	draw_voice_tree(vtree, 1)
+	reset_draw()
+}
+
+func draw_h1(header string) {
+	// Draw box top
+	draw_cell(draw_margin-1, draw_y, 0x250C)
+	draw_cell(draw_margin-1+v_header_width, draw_y, 0x2510)
+	for i := 1; i < v_header_width; i++ {
+		draw_cell(draw_margin-1+i, draw_y, 0x2500)
+	}
+	draw_y++
+
+	draw_cell(draw_margin-1, draw_y, 0x2502)
+	draw_cell(draw_margin-1+v_header_width, draw_y, 0x2502)
+
+	// Draw header text plus sidewalls
+	for idx, letter := range header {
+		draw_cell(draw_margin+idx+1, draw_y, letter)
+	}
+	draw_y++
+
+	// Draw box bottom
+	draw_cell(draw_margin-1, draw_y, 0x2514)
+	draw_cell(draw_margin-1+v_header_width, draw_y, 0x2518)
+	for i := 1; i < v_header_width; i++ {
+		draw_cell(draw_margin-1+i, draw_y, 0x2500)
+	}
+	draw_y++
+}
+
+func draw_voice_tree (tree []*voice_tree, level int) int {
+	//As we draw down, increase max_y
+	level++
+	loc_x := draw_margin + 1 + level
+	total_len_subtrees := 0
+
+	draw_cell(loc_x-3, draw_y-1, 0x252C)
+	for idx, vtree := range tree {
+		// Draw Tree Lines
+		last_line := (idx+1) == len(tree)
+		if last_line {
+			draw_cell(loc_x-3, draw_y, 0x2514)
+		} else {
+			draw_cell(loc_x-3, draw_y, 0x251C)
+		}
+		draw_cell(loc_x-2, draw_y, 0x2500)
+
+		// Draw Shortcut Key
+		draw_cell(loc_x, draw_y, vtree.menu.key)
+		draw_cell(loc_x+1, draw_y, ':')
+
+		// Draw Name
+		for idx, letter := range vtree.menu.descrip {
+			draw_cell(loc_x+3+idx, draw_y, letter)
+		}
+
+		draw_y++
+
+		// Draw Subtree and Line Extension
+		len_subtrees := 0
+		if vtree.tree != nil {
+			len_subtrees += draw_voice_tree(vtree.tree, level)
+			total_len_subtrees += len_subtrees
+			if last_line { continue }
+			// Draw Line Extension
+			for y := 1; y <= len_subtrees; y++ {
+				draw_cell(loc_x-3, draw_y-y, 0x2502)
+			}
+		}
+	}
+
+	return len(tree) + total_len_subtrees
+}
--- soundboard.go
+++ soundboard.go
@@ -2,12 +2,9 @@ package main
 
 import "github.com/nsf/termbox-go"
 
-var draw_margin int = 2
-var draw_y int = draw_margin - 1
-
 type voice_menu struct {
 	key rune
-	title string
+	descrip string
 }
 
 type voice_tree struct {
@@ -15,24 +12,26 @@ type voice_tree struct {
 	tree []*voice_tree
 }
 
-var fg termbox.Attribute = termbox.ColorWhite
-var bg termbox.Attribute = termbox.ColorDefault
-
 var v_header string = "Voice"
-var v_header_width int = 18
 
 var top_tree = []*voice_tree {
 	{voice_menu{'G', "Global"}, global_tree},
 	{voice_menu{'A', "Attack"}, nil},
 	{voice_menu{'D', "Defend"}, nil},
-	{voice_menu{'R', "Repair"}, nil},
+	{voice_menu{'R', "Repair"}, repair_tree},
 	{voice_menu{'B', "Base"}, nil},
 }
 
 var global_tree = []*voice_tree {
 	{voice_menu{'T', "Test"}, action_tree},
 	{voice_menu{'A', "Action"}, nil},
-	{voice_menu{'S', "Super"}, nil},
+	{voice_menu{'S', "Super"}, super_tree},
+}
+
+var super_tree = []*voice_tree {
+	{voice_menu{'C', "Cheer"}, nil},
+	{voice_menu{'L', "Laugh"}, nil},
+	{voice_menu{'W', "Wave"}, nil},
 }
 
 var action_tree = []*voice_tree {
@@ -44,80 +43,9 @@ var attack_tree = []*voice_tree {
 	{voice_menu{'F', "Flag"}, nil},
 }
 
-func draw_h1(header string) {
-	// Draw Box
-	termbox.SetCell(draw_margin-1, draw_y, 0x250C, fg, bg)
-	termbox.SetCell(draw_margin-1+v_header_width, draw_y, 0x2510, fg, bg)
-	for i := 1; i < v_header_width; i++ {
-		termbox.SetCell(draw_margin-1+i, draw_y, 0x2500, fg, bg)
-	}
-	//termbox.SetCell(draw_margin-1, draw_y, 0x250C, fg, bg)
-	draw_y++
-
-	termbox.SetCell(draw_margin-1, draw_y, 0x2502, fg, bg)
-	termbox.SetCell(draw_margin-1+v_header_width, draw_y, 0x2502, fg, bg)
-	// Draw Header
-	for idx, letter := range header {
-		termbox.SetCell(draw_margin+idx+1, draw_y, letter, fg, bg)
-	}
-	draw_y++
-
-	termbox.SetCell(draw_margin-1, draw_y, 0x2514, fg, bg)
-	termbox.SetCell(draw_margin-1+v_header_width, draw_y, 0x2518, fg, bg)
-	for i := 1; i < v_header_width; i++ {
-		termbox.SetCell(draw_margin-1+i, draw_y, 0x2500, fg, bg)
-	}
-	draw_y++
-}
-
-func draw_voice_tree (tree []*voice_tree, level int) int {
-	//As we draw down, increase max_y
-	level++
-	loc_x := draw_margin + 1 + level
-	len_subtrees := 0
-
-	len_tree := len(tree)
-	termbox.SetCell(loc_x-3, draw_y-1, 0x252C, fg, bg)
-	for idx, vtree := range tree {
-		// Draw Tree Lines
-		last_line := (idx+1) == len_tree
-		if last_line {
-			termbox.SetCell(loc_x-3, draw_y, 0x2514, fg, bg)
-		} else {
-			termbox.SetCell(loc_x-3, draw_y, 0x251C, fg, bg)
-		}
-		termbox.SetCell(loc_x-2, draw_y, 0x2500, fg, bg)
-
-		// Draw Shortcut Key
-		termbox.SetCell(loc_x, draw_y, vtree.menu.key, fg, bg)
-		termbox.SetCell(loc_x+1, draw_y, ':', fg, bg)
-
-		// Draw Name
-		for idx, letter := range vtree.menu.title {
-			termbox.SetCell(loc_x+3+idx, draw_y, letter, fg, bg)
-		}
-
-		draw_y++
-
-		// Draw Subtree and Line Extension
-		if vtree.tree != nil {
-			len_subtrees += draw_voice_tree(vtree.tree, level)
-			// Draw Line Extension
-			for y := 1; y <= len_subtrees; y++ {
-				termbox.SetCell(loc_x-3, draw_y-y, 0x2502, fg, bg)
-			}
-		}
-	}
-
-	return len_tree + len_subtrees
-}
-
-func draw_soundboard(header string, vtree []*voice_tree) {
-	//Menu Header
-	draw_h1(header)
-
-	// Menu Lines
-	draw_voice_tree(vtree, 1)
+var repair_tree = []*voice_tree {
+	{voice_menu{'F', "Flag"}, nil},
+	{voice_menu{'B', "Base"}, nil},
 }
 
 func main() {
